bfb5cb8ee0cf193c45376cd4420dd12dfb14fdd1
Jed <jedpadro@gmail.com>
2025-04-20 12:21:53 +0800

    changed if statements to dictionary

--- TrainSMARTApp/LoginForm.cs
+++ TrainSMARTApp/LoginForm.cs
@@ -171,7 +171,35 @@ namespace TrainSMARTApp
 
         private void ShowControls(Panel panel)
         {
-            // TODO: pinpoint control locations in Login panel
+
+            var labelPositions = new Dictionary<Label, Point>
+            {
+                { label_Main_Welcome, new Point(70, 231) },
+                { label_Main_Description, new Point(70, 269) },
+                { label_Register_SignUp, new Point(140, 125) },
+                { label_Register_PrivacyPolicy, new Point(144, 504) },
+                { label_Login_Login, new Point(140, 150) }
+            };
+
+            var cuiButtonPositions = new Dictionary<cuiButton, Point>
+            {
+                { cuiButton_Main_Login, new Point(277, 325) },
+                { cuiButton_Main_Register, new Point(172, 325) },
+                { cuiButton_Register_SignUp, new Point(144, 438) },
+                { cuiButton_Register_PrivacyPolicy, new Point(350, 500) },
+                { cuiButton_Login_ResetPass, new Point(168, 403) },
+                { cuiButton_Login_Login, new Point(346, 402) }
+            };
+
+            var cuiTextBoxPositions= new Dictionary<cuiTextBox2, Point>
+            {
+                { cuiTextBox_Register_Username, new Point(144, 175) },
+                { cuiTextBox_Register_Password, new Point(144, 260) },
+                { cuiTextBox_Register_Email, new Point(144, 345) },
+                { cuiTextBox_Login_Username, new Point(146, 206) },
+                { cuiTextBox_Login_Password, new Point(146, 300) }
+            };
+
             foreach (Control control in panel.Controls)
             {
                 switch (control)
@@ -179,16 +207,10 @@ namespace TrainSMARTApp
                     case Label lbl:
                         lbl.Parent = panel;
                         lbl.BackColor = Color.Transparent;
-                        if (lbl == label_Main_Welcome)
-                            lbl.Location = new Point(70, 231);
-                        if (lbl == label_Main_Description)
-                            lbl.Location = new Point(70, 269);
-                        if (lbl == label_Register_SignUp)
-                            lbl.Location = new Point(140, 125);
-                        if (lbl == label_Register_PrivacyPolicy)
-                            lbl.Location = new Point(144, 504);
-                        if (lbl == label_Login_Login)
-                            lbl.Location = new Point(140, 150);
+                        if (labelPositions.TryGetValue(lbl, out Point pos))
+                        {
+                            lbl.Location = pos;
+                        }
                         break;
 
                     case cuiButton cuiBtn:
@@ -200,12 +222,6 @@ namespace TrainSMARTApp
                         {
                             cuiBtn.HoverBackground = Color.Transparent;
                             cuiBtn.PressedBackground = Color.Transparent;
-                            if (cuiBtn == cuiButton_Main_Register)
-                                cuiBtn.Location = new Point(172, 327);
-                            if (cuiBtn == cuiButton_Register_PrivacyPolicy)
-                                cuiBtn.Location = new Point(350, 500);
-                            if (cuiBtn == cuiButton_Login_ResetPass)
-                                cuiBtn.Location = new Point(168, 403);
                         }
                         if (cuiBtn == cuiButton_Main_Login 
                             || cuiBtn == cuiButton_Register_SignUp
@@ -213,28 +229,20 @@ namespace TrainSMARTApp
                         {
                             cuiBtn.HoverBackground = Color.LightSkyBlue;
                             cuiBtn.PressedBackground = Color.FromArgb(101, 188, 255);
-                            if (cuiBtn == cuiButton_Main_Login)
-                                cuiBtn.Location = new Point(277, 325);
-                            if (cuiBtn == cuiButton_Register_SignUp)
-                                cuiBtn.Location = new Point(144, 438);
-                            if (cuiBtn == cuiButton_Login_Login)
-                                cuiBtn.Location = new Point(346, 402);
+                        }
+                        if (cuiButtonPositions.TryGetValue(cuiBtn, out Point btnLocation))
+                        {
+                            cuiBtn.Location = btnLocation;
                         }
                         break;
 
                     case cuiTextBox2 cuiTxtBx:
                         cuiTxtBx.Parent = panel;
                         cuiTxtBx.BackgroundColor = Color.FromArgb(10, Color.FromArgb(68, 68, 77));
-                        if (cuiTxtBx == cuiTextBox_Register_Username)
-                            cuiTxtBx.Location = new Point(144, 175);
-                        if (cuiTxtBx == cuiTextBox_Register_Password)
-                            cuiTxtBx.Location = new Point(144, 260);
-                        if (cuiTxtBx == cuiTextBox_Register_Email)
-                            cuiTxtBx.Location = new Point(144, 345);
-                        if (cuiTxtBx == cuiTextBox_Login_Username)
-                            cuiTxtBx.Location = new Point(146, 206);
-                        if (cuiTxtBx == cuiTextBox_Login_Password)
-                            cuiTxtBx.Location = new Point(146, 300);
+                        if (cuiTextBoxPositions.TryGetValue(cuiTxtBx, out Point txtLocation))
+                        {
+                            cuiTxtBx.Location = txtLocation;
+                        }
                         break;
                 }
 
